7cce830ea5f82359d01a78da7cb50f5876b89f7a
Aaron <aaronconnelly110@gmail.com>
2024-05-26 16:24:58 -0600

    Fix An Old Friend

--- src/music/AnOldFriend.js
+++ src/music/AnOldFriend.js
@@ -20,23 +20,23 @@ w: age. it| once was new And|
 [V:V2] z2 "Em"[GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|z2 [GBe]2 z2 [GBe]2 z2 "D"[FAd]2 z2 [FAd]2|
 [V:Va] E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|
 #
-[V:V1] E3F G2A2 G2F2 E2D2|
-w: filled with jui- cy tid- bits you could|
-[V:V2] z2 "Em"[GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|E3F G2E2 F3E D2F2|
-[V:Va] E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|E3F G2E2 F3E D2F2|
+[V:V1] E3F G2A2 G2F2 E2D2| C8 {DC} B,8-|
+w: filled with jui- cy tid- bits you could|chew _ |
+[V:V2] z2 "Em"[GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|"Am"z2 [GBe]2 z2 [GBe]2 z2 "Em"[GBe]2 z2 [GBe]2|
+[V:Va] E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|A,,2 z2 E,,2 z2 E,2 z2 B,,2 z2|
 #
-[V:V1] C8 {DC} B,8-|B,12 B,2B,2|
-w: chew _ |_ We de
-[V:V2] "Am"z2 [GBe]2 z2 [GBe]2 z2 "Em"[GBe]2 z2 [GBe]2|z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|
-[V:Va] A,,2 z2 E,,2 z2 E,2 z2 B,,2 z2|E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|
+[V:V1] B,12 B,2B,2|E3B, B,2B,2 F2B,2 B,2B,2|
+w: _ We de|light- ed in the op- ning and the|
+[V:V2] z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2| "Em"z2 [GBe]2 z2 [GBe]2 z2 "B7"[FGB^d]2 z2 [FGB^d]2|
+[V:Va] E,2 z2 B,,2 z2 E,2 z2 B,,2 z2| E,2 z2 B,,2 z2 B,,2 z2 F,,2 z2|
 #
-[V:V1]E3B, B,2B,2 F2B,2 B,2B,2| G2F2 E2D2 E8-|
-w: light- ed in the op- ning and the| clo- sing of the door|
-[V:V2] "Em"z2 [GBe]2 z2 [GBe]2 z2 "B7"[FGB^d]2 z2 [FGB^d]2|"Em"z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|
-[V:Va] E,2 z2 B,,2 z2 B,,2 z2 F,,2 z2|E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|
+[V:V1] G2F2 E2D2 E8-| E12 B,2B,2|
+w: clo- sing of the door| _ And we|
+[V:V2] "Em"z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2| z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|
+[V:Va] E,2 z2 B,,2 z2 E,2 z2 B,,2 z2| E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|
 #
-[V:V1]E12 B,2B,2|E3F G2E2 F3E D2F2|
-w: _ And we|found it so a- ma- zing that the | 
-[V:V2] z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2z2 [GBe]2 z2 [GBe]2 z2 "D"[FAd]2 z2 [FAd]2|
-[V:Va] E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|
+[V:V1] E3F G2E2 F3E D2F2|
+w: found it so a- ma- zing that the |
+[V:V2] z2 [GBe]2 z2 [GBe]2 z2 "D"[FAd]2 z2 [FAd]2|
+[V:Va] E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|
 \`\`\``;
