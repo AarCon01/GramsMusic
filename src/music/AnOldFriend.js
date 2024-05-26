b0644ff8a9d946750a6e4ef81c8a91ec0cbb50c9
Aaron <aaronconnelly110@gmail.com>
2024-05-26 16:07:21 -0600

    Add the first two pages of An Old Friend

---
+++ src/music/AnOldFriend.js
@@ -0,0 +1,42 @@
+export const AnOldFriend = `
+\`\`\`abc
+X: 1
+T: An Old Friend
+M: 4/4
+L: 1/16
+R: reel
+K: Gmaj
+V: V1 clef=treble
+V: V2 clef=treble
+V: Va clef=bass
+#
+[V:V1] B,4|E3F G2E2 F3E D2F2|E2D2 C2D2  E2D2 C2A,2|
+w: There | is an old re fridg er a tor| way up in the back of our gar|
+[V:V2] z4|z2 "Em"[GBe]2 z2 [GBe]2 z2 "D"[FAd]2 z2 [FAd]2|z2 "C"[EGc]2 z2 [EGc]2 z2 [EGc]2 z2 [EGc]2|
+[V:Va] z4|E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|C,2 z2 G,,2 z2 C,2 z2 G,,2 z2|
+#
+[V:V1] B,12 z2B,2|E4 G4 F6 B,2|
+w: age. it| once was new And|
+[V:V2] z2 "Em"[GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|z2 [GBe]2 z2 [GBe]2 z2 "D"[FAd]2 z2 [FAd]2|
+[V:Va] E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|
+#
+[V:V1] E3F G2A2 G2F2 E2D2|
+w: filled with jui- cy tid- bits you could|
+[V:V2] z2 "Em"[GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|E3F G2E2 F3E D2F2|
+[V:Va] E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|E3F G2E2 F3E D2F2|
+#
+[V:V1] C8 {DC} B,8-|B,12 B,2B,2|
+w: chew _ |_ We de
+[V:V2] "Am"z2 [GBe]2 z2 [GBe]2 z2 "Em"[GBe]2 z2 [GBe]2|z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|
+[V:Va] A,,2 z2 E,,2 z2 E,2 z2 B,,2 z2|E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|
+#
+[V:V1]E3B, B,2B,2 F2B,2 B,2B,2| G2F2 E2D2 E8-|
+w: light- ed in the op- ning and the| clo- sing of the door|
+[V:V2] "Em"z2 [GBe]2 z2 [GBe]2 z2 "B7"[FGB^d]2 z2 [FGB^d]2|"Em"z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2|
+[V:Va] E,2 z2 B,,2 z2 B,,2 z2 F,,2 z2|E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|
+#
+[V:V1]E12 B,2B,2|E3F G2E2 F3E D2F2|
+w: _ And we|found it so a- ma- zing that the | 
+[V:V2] z2 [GBe]2 z2 [GBe]2 z2 [GBe]2 z2 [GBe]2z2 [GBe]2 z2 [GBe]2 z2 "D"[FAd]2 z2 [FAd]2|
+[V:Va] E,2 z2 B,,2 z2 E,2 z2 B,,2 z2|E,2 z2 B,,2 z2 D,2 z2 A,,2 z2|
+\`\`\``;
